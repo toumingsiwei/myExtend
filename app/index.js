1a8a736c389f3dfcbf233acb3c29906d94c5d64c
zimian.jt <zimian.jt@alibaba-inc.com>
2015-05-18 17:10:54 +0800

    extend test code

---
+++ app/index.js
@@ -0,0 +1,51 @@
+﻿var Car = function (name, brand, type, output) {
+
+    var args = [].slice.call(arguments);
+    this.name = name;
+    this.brand = brand;
+    this.type = type;    
+    this.output = output;
+
+    this.catetory = 'car';
+}
+Car.prototype.openLight = function () {
+    this.lightStatus = 'open';
+    console.log(this.name + ' lights open');
+}
+Car.prototype.closeLight = function () {
+    this.lightStatus = 'close';
+    console.log(this.name + ' lights up');
+}
+Car.prototype.start = function () {
+    this.enginStatus = 'start';
+    console.log(this.name + ' engin start');
+}
+
+var Cabrio = window.Extend(Car, {
+    openTop: function () {
+        this.topStatus = 'open';
+        console.log(this.name + ' top open');
+    },
+    closeTop: function () {
+        this.topStatus = 'close';
+        console.log(this.name + ' top close');
+    },
+    start: function () {
+        this.enginStatus = 'start';
+        console.log(this.name + ' yeah, let\'s rock');
+    }
+}, function () {
+    this.catetory = 'cabrio';
+});
+
+var car1 = new Car('xiao4', 'audi', 'a4l', '1984');
+car1.openLight();
+car1.closeLight();
+car1.start();
+
+var car2 = new Cabrio('whatever', 'bmw', '650i', '5700');
+car2.openLight();
+car2.closeLight();
+car2.openTop();
+car2.closeTop();
+car2.start();
---
+++ app/libs/myExtend.js
@@ -0,0 +1,22 @@
+﻿(function (glo) {
+    if (!glo.Extend) {
+        glo.Extend = function (func, pro, init) {            
+            if (typeof func === 'function') {
+                var tmplFun = function () {
+                    func.prototype.constructor.apply(this, [].slice.call(arguments));
+                    if (typeof init === 'function') {
+                        init.prototype.constructor.call(this);
+                    }
+                };
+                tmplFun.prototype = new func();
+                if (typeof pro === 'object') {                    
+                    _.extend(tmplFun.prototype, pro);
+                }
+                tmplFun.prototype.constructor = tmplFun;
+                return tmplFun;
+            }
+            return function () { };
+        }
+
+    }
+})(window);
